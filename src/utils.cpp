e4a8b2a16aab9ea2be7b4e55ed4b03391ae72708
Bernardo <103369540+Bernardo46-2@users.noreply.github.com>
2024-11-09 23:05:07 -0300

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,6 +3,7 @@
 #include <numeric>
 #include <random>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
@@ -40,3 +41,50 @@ float calculateAccuracy(const float_vector& y_true, const float_vector& y_pred) 
     }
     return static_cast<float>(correct) / y_true.size();
 }
+
+#ifdef ENABLE_MPI
+
+void serialize_vector(vector<char>& buf, vector<float>& vec) {
+    size_t size = vec.size();
+    char* ptr = reinterpret_cast<char*>(&size);
+    buf.insert(buf.end(), ptr, ptr + sizeof(size_t));
+    buf.insert(buf.end(), reinterpret_cast<char*>(vec.data()), reinterpret_cast<char*>(vec.data()) + vec.size() * sizeof(float));
+}
+
+void serialize_matrix(vector<char>& buf, vector<vector<float>>& vec) {
+    size_t size = vec.size();
+    char* ptr = reinterpret_cast<char*>(&size);
+    buf.insert(buf.end(), ptr, ptr + sizeof(size_t));
+    for(size_t i = 0; i < size; i++) {
+        serialize_vector(buf, vec[i]);
+    }
+}
+
+void deserialize_vector(vector<char>& buf, vector<float>& vec, size_t& idx) {
+    size_t size;
+    memcpy(&size, &buf[idx], sizeof(size_t));
+    idx += sizeof(size_t);
+    vec.resize(size);
+
+    memcpy(vec.data(), &buf[idx], size * sizeof(float));
+    idx += size * sizeof(float);
+}
+
+void deserialize_matrix(vector<char>& buf, vector<vector<float>>& vec, size_t& idx) {
+    size_t matrix_size;
+    memcpy(&matrix_size, &buf[idx], sizeof(size_t));
+    idx += sizeof(size_t);
+    vec.resize(matrix_size);
+    
+    for(size_t i = 0; i < matrix_size; i++) {
+        size_t row_size;
+        memcpy(&row_size, &buf[idx], sizeof(size_t));
+        idx += sizeof(size_t);
+        vec[i].resize(row_size);
+
+        memcpy(vec[i].data(), &buf[idx], row_size * sizeof(float));
+        idx += row_size * sizeof(float);
+    }
+}
+
+#endif
