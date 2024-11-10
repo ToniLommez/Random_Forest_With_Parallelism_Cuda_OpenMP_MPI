e248b66ff6e6d105f456892c3d2de80308a77796
Bernardo <103369540+Bernardo46-2@users.noreply.github.com>
2024-11-09 23:02:49 -0300

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+#define float_tesseract vector<vector<vector<vector<float>>>>
+#define float_cube vector<vector<vector<float>>>
 #define float_matrix vector<vector<float>>
 #define float_vector vector<float>
 
@@ -15,5 +17,12 @@ void trainTestSplit(const float_matrix& X, const float_vector& y,
                     int random_state, float test_size);
 
 float calculateAccuracy(const float_vector& y_true, const float_vector& y_pred);
+
+#ifdef ENABLE_MPI
+void serialize_vector(vector<char>& buf, vector<float>& vec);
+void serialize_matrix(vector<char>& buf, vector<vector<float>>& vec);
+void deserialize_vector(vector<char>& buf, vector<float>& vec, size_t& idx);
+void deserialize_matrix(vector<char>& buf, vector<vector<float>>& vec, size_t& idx);
+#endif
 
 #endif
