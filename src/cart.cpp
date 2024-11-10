43815adef531d0b4c168dd1feeead7432b138c74
Bernardo <103369540+Bernardo46-2@users.noreply.github.com>
2024-11-09 23:03:07 -0300

    Update cart.cpp

--- src/cart.cpp
+++ src/cart.cpp
@@ -1,190 +1,179 @@
+#include "../include/RandomForest.hpp"
 #include "../include/cart.hpp"
+#include "../include/utils.hpp"
 #include <algorithm>
-#include <limits>
-#include <numeric>
+#include <random>
 #include <unordered_map>
-
-#if defined(OMP) || defined(ENABLE_MPI)
-#include <omp.h>
-#endif
+#include <vector>
+#include <iostream>
 
 #ifdef ENABLE_MPI
 #include <mpi.h>
+#define MPI_N_PROCESSES 1
 #endif
-
 
 using namespace std;
 
-Cart::Cart() {
-    Node *tree = nullptr;
+// Construtor
+Random_Forest::Random_Forest(int n_trees, int max_depth, int min_samples_split, bool isClassification)
+    : n_trees(n_trees), max_depth(max_depth), min_samples_split(min_samples_split), isClassification(isClassification) {
+#ifdef ENABLE_MPI
+    int trees_per_process = n_trees / MPI_N_PROCESSES;
+    int remaining_trees = n_trees % MPI_N_PROCESSES;
+    int n_trees_for_main = trees_per_process + (remaining_trees > 0 ? 1 : 0);
+    
+    for(int i = 1; i < MPI_N_PROCESSES; ++i) {
+        int n_trees_to_send = trees_per_process + (i <= remaining_trees ? 1 : 0);
+        MPI_Send(&n_trees_to_send, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 
-    n_features = 0;
-    max_depth = 15;
-    min_samples_split = 10;
-    isClassification = true;
-}
-
-Cart::Cart(const int max_depth, const int min_samples_split, const bool isClassification)
-    : isClassification(isClassification), max_depth(max_depth), min_samples_split(min_samples_split) {
-    Node *tree = nullptr;
-}
-
-Cart::~Cart() {
-    delete root;
-}
-
-void Cart::fit(float_matrix &X_train, float_vector &y_train) {
-    this->X_train = X_train;
-    this->y_train = y_train;
-    this->n_features = X_train[0].size();
-    this->root = split_node(X_train, y_train, 0);
-}
-
-Node *Cart::split_node(const float_matrix &X, const float_vector &y, const int depth) {
-    // Stop condition
-    if (depth >= max_depth || y.size() < min_samples_split || is_pure(y)) {
-        return new_leaf(y);
-    }
-
-    // Best division
-    auto [feature, threshold] = best_threshold(X, y);
-
-    // Divide data
-    auto [X_left, y_left, X_right, y_right] = divide(X, y, feature, threshold);
-
-    // Create nodes
-    Node *node = new Node(feature, false, threshold, 0);
-    node->left = split_node(X_left, y_left, depth + 1);
-    node->right = split_node(X_right, y_right, depth + 1);
-
-    return node;
-}
-
-// isPure test if all values present are from the same type
-bool Cart::is_pure(const float_vector &y) {
-    if (y.empty()) return true;
-
-    for (size_t i = 1; i < y.size(); ++i) {
-        if (y[i] != y[0]) {
-            return false;
+        for(int j = 0; j < n_trees_to_send; j++) {
+            MPI_Send(&max_depth, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&min_samples_split, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&isClassification, 1, MPI_CHAR, i, 0, MPI_COMM_WORLD);
         }
     }
-    return true;
+
+    for(int i = 0; i < n_trees_for_main; i++) {
+        trees.push_back(new Cart(max_depth, min_samples_split, isClassification));
+    }
+#else
+    for(int i = 0; i < n_trees; i++) {
+        trees.push_back(new Cart(max_depth, min_samples_split, isClassification));
+    }
+#endif
 }
 
-float Cart::gini(const float_vector &y) {
-    // number of occurrences per class
-    unordered_map<float, int> class_count;
-    for (float label : y) {
-        class_count[label]++;
-    }
-
-    // calculate gini
-    float gini = 1.0;
-    int total = y.size();
-    for (const auto &[classe, count] : class_count) {
-        float prob = static_cast<float>(count) / total;
-        gini -= prob * prob;
-    }
-    return gini;
+Random_Forest::Random_Forest()
+    : n_trees(100), max_depth(15), min_samples_split(2), isClassification(true) {
 }
 
-pair<int, float> Cart::best_threshold(const float_matrix &X, const float_vector &y) {
-    int best_feature = -1;
-    float best_threshold = 0;
-    float lowest_impurity = numeric_limits<float>::max();
-
-    #if defined(OMP) || defined(ENABLE_MPI)
-    #pragma omp parallel for schedule(dynamic)
-    #endif
-    for (size_t feature = 0; feature < X[0].size(); ++feature) {
-        for (size_t i = 0; i < X.size(); ++i) {
-            float threshold = X[i][feature];
-
-            auto [X_left, y_left, X_right, y_right] = divide(X, y, feature, threshold);
-
-            // Calculate gini value for every group
-            float gini_left = gini(y_left);
-            float gini_right = gini(y_right);
-            float weighted_impurity = (y_left.size() * gini_left + y_right.size() * gini_right) / y.size();
-
-            // Critical section to update shared variables
-            #if defined(OMP) || defined(ENABLE_MPI)
-            #pragma omp critical
-            {
-                // is it the lowest impurity?
-                if (weighted_impurity < lowest_impurity) {
-                    lowest_impurity = weighted_impurity;
-                    best_feature = feature;
-                    best_threshold = threshold;
-                }
-            }
-            #else
-            // is it the lowest impurity?
-            if (weighted_impurity < lowest_impurity) {
-                lowest_impurity = weighted_impurity;
-                best_feature = feature;
-                best_threshold = threshold;
-            }
-            #endif
-        }
+// Destrutor
+Random_Forest::~Random_Forest() {
+#ifndef ENABLE_MPI
+    for (Cart *tree : trees) {
+        delete tree;
     }
-    return {best_feature, best_threshold};
+#endif
 }
 
-tuple<float_matrix, float_vector, float_matrix, float_vector> Cart::divide(const float_matrix &X, const float_vector &y, int feature, float threshold) {
-    float_matrix X_left, X_right;
-    float_vector y_left, y_right;
+// Função de amostragem com reposição (bootstrap)
+pair<float_matrix, float_vector> Random_Forest::bootstrap_sample(const float_matrix &X, const float_vector &y) {
+    float_matrix X_sample;
+    float_vector y_sample;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, X.size() - 1);
 
     for (size_t i = 0; i < X.size(); ++i) {
-        if (X[i][feature] <= threshold) {
-            X_left.push_back(X[i]);
-            y_left.push_back(y[i]);
-        } else {
-            X_right.push_back(X[i]);
-            y_right.push_back(y[i]);
+        int index = dis(gen);
+        X_sample.push_back(X[index]);
+        y_sample.push_back(y[index]);
+    }
+    return {X_sample, y_sample};
+}
+
+// Train
+void Random_Forest::fit(float_matrix &X_train, float_vector &y_train) {
+#ifdef ENABLE_MPI
+    int trees_per_process = n_trees / MPI_N_PROCESSES;
+    int remaining_trees = n_trees % MPI_N_PROCESSES;
+    int n_trees_for_main = trees_per_process + (remaining_trees > 0 ? 1 : 0);
+        
+    for(int i = 1; i < MPI_N_PROCESSES; ++i) {
+        int n_trees_to_send = trees_per_process + (i <= remaining_trees ? 1 : 0);
+        
+        for(int j = 0; j < n_trees_to_send; j++) {
+            auto [X_sample, y_sample] = bootstrap_sample(X_train, y_train);
+            // cout << "Process: 0 - X_sample.size(): " << X_sample.size() << endl;
+            
+            vector<char> buf;
+            serialize_matrix(buf, X_sample);
+            serialize_vector(buf, y_sample);
+            int buf_size = buf.size();
+            
+            MPI_Send(&buf_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(buf.data(), buf.size(), MPI_CHAR, i, 0, MPI_COMM_WORLD);
         }
     }
 
-    return {X_left, y_left, X_right, y_right};
+    for(int i = 0; i < n_trees_for_main; i++) {
+        auto [X_sample, y_sample] = bootstrap_sample(X_train, y_train);
+        trees[i]->fit(X_sample, y_sample);
+    }
+#else
+    for (Cart* tree : trees) {
+        auto [X_sample, y_sample] = bootstrap_sample(X_train, y_train);
+        tree->fit(X_sample, y_sample);
+    }
+#endif
 }
 
-Node *Cart::new_leaf(const float_vector &y) {
-    Node *leaf = new Node();
-    leaf->is_leaf = true;
+// Predict
+float_vector Random_Forest::predict(float_matrix &X_test) {
+    vector<float_vector> all_predictions;
 
-    if (isClassification) {
-        std::unordered_map<float, int> classCounts;
-        for (float label : y) {
-            classCounts[label]++;
-        }
-        leaf->prediction = max_element(classCounts.begin(), classCounts.end(), [](const auto &a, const auto &b) { return a.second < b.second; })->first;
-    } else {
-        float sum = accumulate(y.begin(), y.end(), 0.0f);
-        leaf->prediction = sum / y.size();
+#ifdef ENABLE_MPI
+    int trees_per_process = n_trees / MPI_N_PROCESSES;
+    int remaining_trees = n_trees % MPI_N_PROCESSES;
+    int n_trees_for_main = trees_per_process + (remaining_trees > 0 ? 1 : 0);
+    
+    for(int i = 1; i < MPI_N_PROCESSES; i++) {
+        vector<char> buf;
+        serialize_matrix(buf, X_test);
+        int buf_size = buf.size();
+        MPI_Send(&buf_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        MPI_Send(buf.data(), buf.size(), MPI_CHAR, i, 0, MPI_COMM_WORLD);
     }
 
-    return leaf;
-}
+    for(int i = 0; i < n_trees_for_main; i++) {
+        trees_preds.push_back(trees[i]->predict(X_test));
+    }
 
-float_vector Cart::predict(const float_matrix& X_test) {
+    for(int i = 1; i < MPI_N_PROCESSES; i++) {
+        int n_trees_to_send = trees_per_process + (i <= remaining_trees ? 1 : 0);
+        
+        for(int j = 0; j < n_trees_to_send; j++) {
+            int buf_size;
+            vector<char> buf;
+            vector<float> pred;
+            size_t idx = 0;
+            
+            MPI_Recv(&buf_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            buf.resize(buf_size);
+            MPI_Recv(buf.data(), buf_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            deserialize_vector(buf, pred, idx);
+            all_predictions.push_back(pred);
+        }
+    }
+
+    for(int i = 0; i < n_trees_for_main; i++) {
+        all_predictions.push_back(trees_preds[i]);
+    }
+#else
+    for(Cart* tree : trees) {
+        all_predictions.push_back(tree->predict(X_test));
+    }
+#endif
+
     float_vector predictions;
+    for (size_t i = 0; i < X_test.size(); ++i) {
+        unordered_map<float, int> class_counts;
 
-    for (const auto& sample : X_test) {
-        predictions.push_back(predict_single(sample, root));
+        for (const auto &preds : all_predictions) {
+            class_counts[preds[i]]++;
+        }
+
+        // Encontrar a previsão mais comum (voto majoritário)
+        float majority_class = max_element(
+            class_counts.begin(), 
+            class_counts.end(),
+            [](const auto &a, const auto &b) { return a.second < b.second; }
+        )->first;
+        predictions.push_back(majority_class);
     }
 
     return predictions;
-}
-
-float Cart::predict_single(const std::vector<float>& sample, Node* node) {
-    if (node->is_leaf) {
-        return node->prediction;
-    }
-
-    if (sample[node->feature] <= node->threshold) {
-        return predict_single(sample, node->left);
-    } else {
-        return predict_single(sample, node->right);
-    }
 }
